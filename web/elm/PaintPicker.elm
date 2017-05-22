d3d6c2929faa4f6910559d311008552b092f7bd8
Bibek Pandey <bibekp@gmail.com>
2017-05-22 16:54:37 -0700

    Filled in elm initial model with hardcoded list of paints and simple view.  Added a css file to display paint cans

--- web/elm/PaintPicker.elm
+++ web/elm/PaintPicker.elm
@@ -1,7 +1,61 @@
 module PaintPicker exposing (..)
 
-import Html
+import Html exposing (Html, ul, li, text)
+import Html.Attributes exposing (class)
 
 
+main : Html String
 main =
-    Html.text "Hello from Elm"
+    view init
+
+
+
+-- MODEL
+-- Paint is a record containing various attributes
+-- assume these Paints work for interior and exterior and are the best brand
+-- each paint record is 1 gallon
+
+
+type alias Paint =
+    { color : String
+    , sheen : String
+    , picked : Bool
+    }
+
+
+
+-- Model refers to List of Paints
+
+
+type alias Model =
+    List Paint
+
+
+init : Model
+init =
+    [ { color = "salmon", sheen = "gloss", picked = False }
+    , { color = "tomato", sheen = "flat", picked = False }
+    , { color = "darkorange", sheen = "satin", picked = False }
+    , { color = "indianred", sheen = "gloss", picked = False }
+    , { color = "greenyellow", sheen = "eggshell", picked = False }
+    , { color = "mediumspringgreen", sheen = "eggshell", picked = False }
+    , { color = "khaki", sheen = "flat", picked = False }
+    , { color = "gold", sheen = "flat", picked = False }
+    , { color = "rosybrown", sheen = "satin", picked = False }
+    , { color = "teal", sheen = "semi-gloss", picked = False }
+    , { color = "maroon", sheen = "semi-gloss", picked = False }
+    ]
+
+
+
+-- VIEW
+
+
+view : Model -> Html String
+view model =
+    ul [ class "paints" ] (List.map paintSingle model)
+
+
+paintSingle : Paint -> Html String
+paintSingle paint =
+    li [ class "paint available" ] [ text paint.color ]
